26ac1274b7115ef94ab24d430d3aa1797c218212
Reddy <Agnesh.Reddy.ctr@sabre.com>
2019-10-11 11:22:27 +0530

    admin

---
+++ src/Admin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Admin.css';
+
+function Admin() {
+  return (
+    <div className="Admin">
+      <header className="Admin-header">
+        Vending Machine - Admin
+      </header>
+    </div>
+  );
+}
+
+export default Admin;
